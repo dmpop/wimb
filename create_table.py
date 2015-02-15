94aee87998f1caad5d474b01c297196efff581b7
Dmitri Popov <dmpop@linux.com>
2015-02-15 23:55:02 +0100

    thinkpad-t410-kubuntu

--- create_table.py
+++ create_table.py
@@ -2,5 +2,5 @@
 import sqlite3
 con = sqlite3.connect('wimb.sqlite') # Warning: This file is created in the current directory
 con.execute("CREATE TABLE wimb (id INTEGER PRIMARY KEY, item char(254) NOT NULL, serial_no char(100))")
-con.execute("INSERT INTO wimb (item,serial_no) VALUES ('Nikon Nikomat FTn','FT3855032')")
+con.execute("INSERT INTO wimb (item,serial_no) VALUES ('Nippon Kogaku K. K. Nikomat FTn','FT3855032')")
 con.commit()
